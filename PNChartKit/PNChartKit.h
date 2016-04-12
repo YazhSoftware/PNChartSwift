cd495f1b1e34f9e9110968e20ec0dda8255fbd1f
kRanganadhan <Karunanithi.Ranganadhan-e@maf.ae>
2016-04-12 18:10:26 +0400

    changes.

--- PNChartKit/PNChartKit.h
+++ PNChartKit/PNChartKit.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char PNChartKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PNChartKit/PublicHeader.h>
 
+#import <PNChartKit/PNChart.h.h>
 
