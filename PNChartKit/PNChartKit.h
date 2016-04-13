1494595e91e7ebb20a7f12e6112168b85ba3185a
kRanganadhan <Karunanithi.Ranganadhan-e@maf.ae>
2016-04-13 09:30:33 +0400

    changes.

--- PNChartKit/PNChartKit.h
+++ PNChartKit/PNChartKit.h
@@ -2,7 +2,7 @@
 //  PNChartKit.h
 //  PNChartKit
 //
-//  Created by karuna on 4/12/16.
+//  Created by karuna on 4/13/16.
 //  Copyright © 2016 kevinzhow. All rights reserved.
 //
 
@@ -16,16 +16,4 @@ FOUNDATION_EXPORT const unsigned char PNChartKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PNChartKit/PublicHeader.h>
 
-#import <PNChartKit/PNChart.h>
-#import <PNChartKit/PNColor.h>
-#import <PNChartKit/PNLineChart.h>
-#import <PNChartKit/PNLineChartData.h>
-#import <PNChartKit/PNLineChartDataItem.h>
-#import <PNChartKit/PNBarChart.h>
-#import <PNChartKit/PNCircleChart.h>
-#import <PNChartKit/PNChartDelegate.h>
-#import <PNChartKit/PNPieChart.h>
-#import <PNChartKit/PNScatterChart.h>
-#import <PNChartKit/PNRadarChart.h>
-#import <PNChartKit/PNRadarChartDataItem.h>
 
